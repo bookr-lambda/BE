689f84d2dca682dc3a858cf226f8483f5ed3438b
Henry Neal <henry.neal0@gmail.com>
2019-07-08 17:38:47 -0400

    reviews table

--- data/migrations/20190708160629_reviews.js
+++ data/migrations/20190708160629_reviews.js
@@ -1,6 +1,20 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable("reviews", table => {
+        table.increments("review_id");
+        table.string("user_id")
+            .references("user_id")
+            .inTable("users")
+            .onDelete("CASCADE")
+            .onUpdate("CASCADE");
+        table.text("review");
+        table.float("rating");
+        table.string("book_id")
+            .references("id")
+            .inTable("books")
+            .onDelete("CASCADE")
+            .onUpdate("CASCADE");
+    })
 };
 
 exports.down = function(knex) {
