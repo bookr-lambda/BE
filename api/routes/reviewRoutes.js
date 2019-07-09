fd12b239165a8d30ef9b85ce226d93c58b249942
Henry Neal <henry.neal0@gmail.com>
2019-07-09 15:30:45 -0400

    working on review crud routes

--- api/routes/reviewRoutes.js
+++ api/routes/reviewRoutes.js
@@ -2,6 +2,61 @@ const express = require("express");
 const db = require("../../data/dbConfig");
 const router = express.Router();
 
+// Delete // Delete // Delete // Delete // Delete // Delete // Delete // Delete // Delete 
+server.delete("/reviews/:id", async (req, res) => {
+  try {
+    const review = await db("reviews")
+      .where({ review_id: req.params.review_id })
+      .del();
+    if (review) {
+      res.status(200).json({
+        message: "Review was deleted successfully.", review
+      });
+    } else {
+      res.status(404).json({ message: "The review could not be deleted." });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "There was an error deleting the review.", error });
+  }
+});
+
+
+// Get all // Get all // Get all // Get all // Get all // Get all // Get all // Get all 
+server.get("/reviews", async (req, res) => {
+  try {
+    const reviews = await db("reviews");
+    res.status(200).json(reviews);
+  } catch (error) {
+    res.status(500).json({
+      message: "There was an error retrieving the review from the database.",
+      error
+    });
+  }
+});
+
+// Get Id  // Get Id  // Get Id  // Get Id  // Get Id  // Get Id  // Get Id  // Get Id  
+router.get("/reviews/:id", async (req, res) => {
+  try {
+    const reviews = await db("reviews").where({ id: req.params.id });
+    if (reviews.length) {
+      const review = reviews[0];
+      res.status(200).json({ ...review});
+    } else {
+      res
+        .status(404)
+        .json({ message: "That review could not be found in the database." });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "There was an error retrieving the review from the database.",
+      error
+    });
+  }
+});
+
+// Post // Post // Post // Post // Post // Post // Post // Post // Post // Post // Post 
 router.post("/reviews", async (req, res) => {
     if (!req.body.review) {
       return res.status(400).json({
@@ -14,7 +69,7 @@ router.post("/reviews", async (req, res) => {
       if (review) {
         res
           .status(200)
-          .json({ message: "review created successfully.", review });
+          .json({ message: "Review created successfully.", review });
       } else {
         res
           .status(404)
